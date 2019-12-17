500f9848c308cdcd249c470f5bac131423db3e1a
Kinrezc <matt.czernik@gmail.com>
2019-12-16 22:56:40 -0500

    Remove unused dependencies, begin building theme

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export default {
+  fonts: {
+    primary: "Rubik",
+    secondary: "DINPro"
+  },
+  colors: {
+    green: "#0bd080",
+    red: "#ed1e79",
+    blue: "#2188ea",
+    disabledGrey: "#d1d1d1",
+    white: "#ffffff"
+  },
+  styles: {
+    tokenText: {
+      fontFamily: "secondary",
+      fontSize: 22,
+      fontWeight: "bold",
+      letterSpacing: "1.4px"
+    },
+    buttonTextPrimary: {
+      fontFamily: "primary",
+      fontSize: 14
+    }
+  }
+};
